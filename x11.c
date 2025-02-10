065e159dcb29c4ca00da8ca4769ffbb13ea80052
weasel <paulbongers22@gmail.com>
2025-02-10 20:25:16 +0100

    x

--- x11.c
+++ x11.c
@@ -20,10 +20,24 @@ int main(void)
 					0, 
 					0);
 
+    XWindowAttributes wind_attr = {0};
+    XGetWindowAttributes(display, window, &wind_attr);
+
+    // format == ZPixmap. so we have color, else XYPixmap. not explained in docs btw.
+    XImage *image = XCreateImage(display, 
+				 wind_attr.visual, wind_attr.depth,
+				 ZPixmap,
+				 0,
+				 (char *) grid_px, // TODO: map to game code
+				 GRID_WIDTH_PX, GRID_HEIGHT_PX,
+				 32,
+				 GRID_WIDTH * sizeof(RGBA32));
+
+    GC gc = XCreateGC(display, window, 0, NULL);
+    
     // event-msg we wanna receive from window maager layer. kinda? docs suck. 
     Atom wm_delete_window = XInternAtom(display, "WM_DELETE_WINDOW", False);
-    
-XSetWMProtocols(display, window, &wm_delete_window, 1);
+    XSetWMProtocols(display, window, &wm_delete_window, 1);
 
     XSelectInput(display, window, KeyPressMask);
 
@@ -40,12 +54,13 @@ XSetWMProtocols(display, window, &wm_delete_window, 1);
 	switch (event.type)
 	{
 	    case KeyPress: {
-		printf("got the key press master weasel\n");
+		printf("x_window key pressed master weasel\n");
 	    } break;
 
 	    case ClientMessage: {
-		if ((Atom) event.xclient.data.l[0] == wm_delete_window) // becoz S.O said so
+		if ((Atom) event.xclient.data.l[0] == wm_delete_window) // thnx S.O
 		{
+		    printf("WM_DELETE go bbbrrr");
 		    quit = 1;
 		}
 	    } break;
@@ -54,6 +69,5 @@ XSetWMProtocols(display, window, &wm_delete_window, 1);
 
     XCloseDisplay(display);
     
-    return 0;
-    
+    return 0;    
 }
